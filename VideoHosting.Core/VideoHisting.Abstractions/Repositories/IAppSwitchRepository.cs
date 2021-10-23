78670e3689a2c52a9056404cc0a463a08ed2b376
Victor Dyshkant <Victor_Dyshkant@epam.com>
2021-10-23 12:59:29 +0300

    first commit

--- VideoHosting.Core/VideoHisting.Abstractions/Repositories/IAppSwitchRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace VideoHosting.Abstractions.Repositories
-{
-    public interface IAppSwitchRepository
-    {
-        string GetValue(string key);
-    }
-}
--- VideoHosting.Core/VideoHosting.Domain/Infrastructure/AppSwitch.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VideoHosting.Domain.Infrastructure
-{
-    public class AppSwitch
-    {
-        public string Key { get; set; } 
-
-        public string Value { get; set; } 
-    }
-}
